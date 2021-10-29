6be5e141e860b9411bb39aea063e5b48fe5b5eac
EnZzei1 <90902544+EnZzei1@users.noreply.github.com>
2021-10-29 11:01:13 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+const Mongodb = require('./mongo.js');
+const config = require('./config.js');
+const search = require('./commands/explore.js');
+const antispam = require('./antispam.js');
+
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const db = new Mongodb(config.mongodb_uri);
+const client = new Discord.Client({
+    messageCacheMaxSize: 1,
+    messageCacheLifetime: 1,
+    messageSweepInterval: 1,
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER']
+});
+
+const DiscordButtons = require("discord-buttons")(client);
+
+const urlCheck = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|png)/i; // url check regexp
+
+const modules = {};
+
+client.login(config.token);
+
+client.on('ready', async () => {
+    await db.init();
+    console.log(`Logged in as ${client.user.tag}`);
+    console.log(`Working with: ${client.guilds.cache.size} guilds`);
+
+    try {
+        fs.readdir('./commands/', (err, files) => {
+            files.map(file => {
+                if (file.split('.')[1] !== 'js') return
+                modules[config.prefix + file.split('.')[0]] = require(`./commands/${file}`);
+                console.log(`${file} was successfully loaded!`);
+            })
+        })
+    } catch (e) {
+        console.log(e);
+    }
+})
+
+client.on('message', async (msg) => {
+    if (msg.author.bot) return
+    let message = msg.content.toLocaleLowerCase();
+    try {
+        let cmd = message.split(' ')[0]
+        if (Object.keys(modules).includes(cmd)) {
+            let isSpammer = antispam.checkUser(msg);
+            if (!isSpammer) {
+                msg.channel.startTyping();
+                return modules[Object.keys(modules)[Object.keys(modules).indexOf(cmd)]].run(client, msg, config, db);
+            }
+
+        }
+
+        let guildData = await db.getGuildSettings(msg.guild.id);
+
+        let attachments = msg.attachments.size ? msg.attachments.first().url : undefined;
+        let imgUrl = msg.content.match(urlCheck) == null ? false : true;
+        let isIncludesImage = attachments ? true : imgUrl
+
+        if (guildData && guildData.settings.workChannel && msg.channel.id == guildData.settings.workChannel && isIncludesImage) {
+            let isSpammer = antispam.checkUser(msg);
+            if (!isSpammer) return search.run(client, msg);
+        } 
+        
+    } catch (err) {
+        console.log(err)
+    } finally {
+        msg.channel.stopTyping();
+    }
+})
